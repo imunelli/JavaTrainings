1cdedb741a689d49c08c9ccd4b7abe266603dbd8
imunelli <imunelli-mdm@ramble.ru>
2015-09-06 19:06:40 +0300

    бла бла бла

---
+++ src/home/First.java
@@ -0,0 +1,11 @@
+package home;
+
+/**
+ * Created by imunelli on 06.09.2015.
+ */
+public class First {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Java");
+    }
+}
